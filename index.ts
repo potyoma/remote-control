0b4cf52bb9a43d242a00a087dfa2aa1aaf6d575c
potyoma <artyomnovosyolov254@gmail.com>
2023-01-23 20:57:10 +0400

    feat: handle incoming messages and exit

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
-import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import { httpServer } from "./src/http_server"
+import handleMessage from "./src/core/handleMessage"
+import { mouse, getActiveWindow } from "@nut-tree/nut-js"
+import { WebSocketServer, createWebSocketStream } from "ws"
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = 8181
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${HTTP_PORT} port!`)
+httpServer.listen(HTTP_PORT)
+
+const wss = new WebSocketServer({ port: 8080 })
+
+wss.on("connection", ws => {
+  const wsStream = createWebSocketStream(ws, { decodeStrings: false })
+  wsStream.on("data", data => {
+    handleMessage(data, wsStream)
+  })
+})
+
+process.on("SIGINT", () => {
+  for (const client of wss.clients.values()) client.close()
+  wss.close(() => httpServer.close(() => process.exit()))
+})
