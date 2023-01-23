48955a336ed6cbcbca3e32dc2a9c72cee502a257
potyoma <artyomnovosyolov254@gmail.com>
2023-01-23 21:04:22 +0400

    feat: message on setting up websocket

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import { httpServer } from "./src/http_server"
 import handleMessage from "./src/core/handleMessage"
-import { mouse, getActiveWindow } from "@nut-tree/nut-js"
 import { WebSocketServer, createWebSocketStream } from "ws"
 
 const HTTP_PORT = 8181
+const WEBSOCKET_PORT = 8080
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`)
 httpServer.listen(HTTP_PORT)
 
-const wss = new WebSocketServer({ port: 8080 })
+const wss = new WebSocketServer({ port: WEBSOCKET_PORT }, () => {
+  console.log(`Start websocket server on the ${WEBSOCKET_PORT}`)
+})
 
 wss.on("connection", ws => {
   const wsStream = createWebSocketStream(ws, { decodeStrings: false })
